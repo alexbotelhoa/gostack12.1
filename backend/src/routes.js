e6384d51c4b81c66bd0e0af3bba0dedc519b3585
Alex Botelho <alexbotelho1@hotmail.com>
2020-06-15 20:05:42 -0300

    Nível 1 - Backend Concluído

---
+++ backend/src/routes.js
@@ -0,0 +1,84 @@
+const { Router } = require('express');
+const { uuid, isUuid } = require('uuidv4')
+
+const routes = Router();
+const projects = [];
+
+function logRequests(req, res, next) {
+    const { method, url } = req;
+
+    const logLabel = `[${method.toUpperCase()}] ${url}`;
+
+    console.time(logLabel);
+
+    next();
+
+    console.timeEnd(logLabel)
+};
+
+function validateProjectId(req, res, next) {
+    const { id } = req.params;
+
+    if (!isUuid(id)) return res.status(400).json({ error: 'Invalid project ID.' });
+
+    return next;
+};
+
+routes.use(logRequests);
+
+routes.get('/', (req, res) =>{
+    res.json({
+        'Hellow': 'World'
+    });
+});
+
+routes.get('/projects', (req, res) => {
+    const { title } = req.query;
+
+    const filtered = title 
+        ? projects.filter(project => project.title.includes(title))
+        : projects;
+
+    return res.status(200).json(filtered);
+});
+
+routes.post('/projects', (req, res) => {
+    const { title, owner } = req.body;
+
+    const project = { id: uuid(), title, owner }
+
+    projects.push(project);
+
+    return res.status(201).json(project);
+});
+
+routes.put('/projects/:id', validateProjectId, (req, res) => {
+    const { id } = req.params;
+    const { title, owner } = req.body;
+
+    const projectIndex = projects.findIndex(project => project.id === id);
+    if (projectIndex < 0) return res.status(400).json({ error: 'Project not fund'});
+
+    const project = {
+        id,
+        title,
+        owner
+    };
+
+    projects[projectIndex] = project;
+
+    return res.status(200).json(project);
+});
+
+routes.delete('/projects/:id', validateProjectId, (req, res) => {
+    const { id } = req.params;
+
+    const projectIndex = projects.findIndex(project => project.id === id);
+    if (projectIndex < 0) return res.status(400).json({ error: 'Project not fund'});
+
+    projects.splice(projectIndex, 1);
+
+    return res.status(204).send();
+});
+
+module.exports = routes;
