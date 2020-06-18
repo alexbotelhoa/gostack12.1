9a2e71ca2a57b0d5b5a1a056f5df7551db7cd824
Alex Botelho <alexbotelho1@hotmail.com>
2020-06-18 10:31:10 -0300

    Preparando Ambiente

--- mobile/src/App.js
+++ mobile/src/App.js
@@ -1,6 +1,30 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Text, StyleSheet, StatusBar } from 'react-native';
 
 export default function App() {
-    return <View />
+    return (
+        <>
+            <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+
+            
+            <View style={styles.container}>
+                <Text style={styles.title}>GoStack 12.0</Text>
+            </View>
+        </>
+    )
 };
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#7159c1',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+
+    title: {
+        color: '#fff',
+        fontSize: 32,
+        fontWeight: 'bold'
+    }
+})
